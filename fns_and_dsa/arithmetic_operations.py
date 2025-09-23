90707ee7c304846743aea375c1394838bc30c47b
Lebohang Radebe <radebelebohang99@gmail.com>
2025-09-23 10:48:35 +0200

    Replace match-case with if-else logic

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,20 +1,16 @@
 def perform_operation(num1, num2, operation):
-    match operation:
-        case 'add':
-            return num1 + num2
-        case 'subtract':
-            return num1 - num2
-        case 'multiply':
-            return num1 * num2
-        case 'divide':
-            if num2 == 0:
-                print('Cannot divide by zero')
-                return 0
-            else:
-                return num1 / num2
-        case _:
-            print('Invalid operation')
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            print('Cannot divide by zero')
             return 0
-
-            
-
+        else:
+            return num1 / num2
+    else:
+        print('Invalid operation')
+        return 0
